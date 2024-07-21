8c3a1a145c8999ebfee4941b03c97f3865b67e9b
Aram <aramsaribekyan1@gmail.com>
2024-07-21 09:14:38 +0400

    Initial Commit

---
+++ ChooseTheFlagGame/ChooseTheFlagGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseTheFlagGameApp.swift
+//  ChooseTheFlagGame
+//
+//  Created by Aram on 21.07.24.
+//
+
+import SwiftUI
+
+@main
+struct ChooseTheFlagGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
